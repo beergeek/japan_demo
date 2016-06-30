05a23c5d568865466939644632355e3be1a7247f
beergeek <brett.gray@puppetlabs.com>
2016-07-01 05:08:01 +1000

    variable error

--- site/profile/manifests/web_services/iis.pp
+++ site/profile/manifests/web_services/iis.pp
@@ -1,6 +1,6 @@
 class profile::web_services::iis {
 
-  $website_hash = hiera('profile::web_services::iis::website_hash')
+  $website_hash = hiera('profile::web_services::iis::website_hash',undef)
   $base_docroot = hiera('profile::web_services::iis::base_docroot')
 
   include ::iis
@@ -16,42 +16,44 @@ class profile::web_services::iis {
     ensure => absent,
   }
 
-  website_hash.each do |String $site_name, Hash website| {
-    $_docroot = "${base_docroot}\\${website['docroot']}"
+  if $website_hash {
+    $website_hash.each |String $site_name, Hash $website| {
+      $_docroot = "${base_docroot}\\${website['docroot']}"
 
-    iis::manage_app_pool { $site_name:
-      enable_32_bit           => true,
-      managed_runtime_version => 'v4.0',
-    }
+      iis::manage_app_pool { $site_name:
+        enable_32_bit           => true,
+        managed_runtime_version => 'v4.0',
+      }
 
-    iis::manage_site { $site_name:
-      site_path   => $_docroot,
-      port        => '80',
-      ip_address  => '*',
-      host_header => $site_name,
-      app_pool    => $site_name,
-      before      => File[$site_name],
-    }
+      iis::manage_site { $site_name:
+        site_path   => $_docroot,
+        port        => '80',
+        ip_address  => '*',
+        host_header => $site_name,
+        app_pool    => $site_name,
+        before      => File[$site_name],
+      }
 
-    acl { $_docroot:
-      target                     => $_docroot,
-      purge                      => false,
-      permissions                => [
-        { identity => 'vagrant', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all' },
-        { identity => 'Administrators', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all'}
-        ],
-        owner                      => 'vagrant',
-        group                      => 'Administrators',
-        inherit_parent_permissions => true,
-    }
-    file { $_docroot:
-      ensure  => directory,
-      owner   => 'vagrant',
-      group   => 'Administrators',
-      recurse => true,
-      purge   => true,
-      force   => true,
-      source  => "puppet:///web_sites/${site_name}",
+      acl { $_docroot:
+        target                     => $_docroot,
+        purge                      => false,
+        permissions                => [
+          { identity => 'vagrant', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all' },
+          { identity => 'Administrators', rights => ['full'], perm_type=> 'allow', child_types => 'all', affects => 'all'}
+          ],
+          owner                      => 'vagrant',
+          group                      => 'Administrators',
+          inherit_parent_permissions => true,
+      }
+      file { $_docroot:
+        ensure  => directory,
+        owner   => 'vagrant',
+        group   => 'Administrators',
+        recurse => true,
+        purge   => true,
+        force   => true,
+        source  => "puppet:///web_sites/${site_name}",
+      }
     }
   }
 }
