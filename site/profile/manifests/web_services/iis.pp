73bf0caa979109bbfe6b4eb32fc78e80b65f9928
beergeek <brett.gray@puppetlabs.com>
2016-07-05 21:45:26 +1000

    correct port

--- site/profile/manifests/web_services/iis.pp
+++ site/profile/manifests/web_services/iis.pp
@@ -135,7 +135,7 @@ class profile::web_services::iis {
           @@haproxy::balancermember { "${site_name}-${::fqdn}":
             listening_service => $site_name,
             server_names      => $::fqdn,
-            ipaddresses       => $::networking['Ehternet 2']['ip'],
+            ipaddresses       => $::networking['Ethernet 2']['ip'],
             ports             => $website['port'],
             options           => 'check',
           }
