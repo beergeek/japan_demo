00502252b0e5d00e7274c994b3a932e4acfee5ab
beergeek <brett.gray@puppetlabs.com>
2016-07-11 19:36:38 +1000

    nagios for vhosts

--- site/profile/manifests/web_services/iis.pp
+++ site/profile/manifests/web_services/iis.pp
@@ -103,7 +103,7 @@ class profile::web_services::iis {
           use                 => 'generic-service',
           host_name           => $::fqdn,
           service_description => "${::fqdn}_http_${site_name}",
-          check_command       => "check_http -H ${site_name} -I ${networking['interfaces']['Ethernet 2']['ip']} -p ${website['port']} -u http://${site_name}",
+          check_command       => "check_http!${site_name} -I ${networking['interfaces']['Ethernet 2']['ip']} -p ${website['port']} -u http://${site_name}",
           target              => "/etc/nagios/conf.d/${::fqdn}_service.cfg",
           notify              => Service['nagios'],
           require             => File["/etc/nagios/conf.d/${::fqdn}_service.cfg"],
